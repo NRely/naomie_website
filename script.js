5ff4fabed9a3a85241d76e05f0720a1a13fe2e02
Nmetelus <nmetelus6@gmail.com>
2021-07-13 19:20:00 -0400

    started wesbite

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('hello!')
+
+document.getElementById("About").onclick = function() {
+  console.log('hello')
+  window.open('www.google.com', '_blank')
+}
+document.getElementById("NextH").onclick=
+function() {
+  console.log('hello1')
+}

0754f81207c3ccd6de38b26218aa972e18e7d457
Nmetelus <nmetelus6@gmail.com>
2021-07-20 18:49:11 -0400

    website

--- script.js
+++ script.js
@@ -4,7 +4,4 @@ document.getElementById("About").onclick = function() {
   console.log('hello')
   window.open('www.google.com', '_blank')
 }
-document.getElementById("NextH").onclick=
-function() {
-  console.log('hello1')
-}
+

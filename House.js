6fe8501cb7a90838f9ff1074f69b68fddec6b92d
Nmetelus <nmetelus6@gmail.com>
2021-08-27 10:46:13 -0400

    Real Estate

---
+++ House.js
@@ -0,0 +1,102 @@
+const Houses = [
+    {
+      image_url: 'GlendaleNY.png',
+      price: '$850,000',
+      sqft: '1,742',
+      bedroom: '3',
+      bathroom: '2',
+      estimated_payment: '$3,700/month',
+      listed_by: 'Rely Estate',
+      address: '80-08 78th Ave, Glendale, NY 11385'
+    },
+      {
+      image_url: 'BaysideNY.png',
+      price: '$875,000',
+      sqft: '2,614',
+      bedroom: '3',
+      bathroom: '1.5',
+      estimated_payment: '$3,901/month',
+      listed_by: 'Rely Estate',
+      address: '67-28 Bell Blvd, Bayside, NY 11364'
+    },
+    {
+      image_url: 'RidgewoodNY.png',
+      price: '$1,150,000',
+      sqft: '1,742',
+      bedroom: '3',
+      bathroom: '3',
+      estimated_payment: '$4,766/month',
+      listed_by: 'Rely Estate',
+      address: '18-79 Suydam St, Ridgewood, NY 11385'
+    },
+    {
+      image_url: 'FlushingNY.png',
+      price: '$1,188,000',
+      sqft: '2,718',
+      bedroom: '6',
+      bathroom: '2.5',
+      estimated_payment: '$5,170/month',
+      listed_by: 'Rely Estate',
+      address: '46-90 Utopia Pkwy, Flushing, NY 11358'
+    },
+    {
+      image_url: 'FloralParkNY.png',
+      price: '$825,000',
+      sqft: '4,792',
+      bedroom: '3',
+      bathroom: '2.5',
+      estimated_payment: '$4,518/month',
+      listed_by: 'Rely Estate',
+      address: '261 Crocus Ave, Floral Park, NY 11001'
+    },
+    {
+      image_url: 'MassapequaNY.png',
+      price: '$1,199,000',
+      sqft: '3,246',
+      bedroom: '4',
+      bathroom: '3.5',
+      estimated_payment: '$6,404/month',
+      listed_by: 'Rely Estate',
+      address: '170 Whitewood Dr, Massapequa Park, NY 11762'
+    },
+  ];
+
+  const container = document.getElementById("container")
+  const textcreator = function (text, housecard) {
+      const housepara = document.createElement("p")
+      const textnode = document.createTextNode(text)
+      housepara.appendChild(textnode);
+      housecard.appendChild(housepara);
+  }
+
+  Houses.map(
+    (house) => {
+      const housecard = document.createElement("div");
+      housecard.className = "housecard";
+
+      const houseimages = document.createElement("img");
+      houseimages.className = "houseimages"
+      houseimages.src = ` ${house.image_url}`;
+      housecard.appendChild(houseimages);
+
+      textcreator(`price: ${house.price}`, housecard);
+      
+      textcreator(`sqft ${house.sqft}`, housecard);
+
+      textcreator(`bedroom: ${house.bedroom}`, housecard);
+
+      textcreator(`bathroom: ${house.bathroom}`, housecard);
+      
+      textcreator(`estimated_payment: ${house.estimated_payment},`, housecard);
+
+      textcreator(`address: ${house.address},`, housecard);
+
+      container.appendChild(housecard); 
+
+    }
+
+  )
+
+  
+
+  
--- script.js
+++ script.js
@@ -1,7 +1,41 @@
-console.log('hello!')
 
-document.getElementById("About").onclick = function() {
-  console.log('hello')
-  window.open('www.google.com', '_blank')
+
+
+const NavigationSlide = ()=> {
+  const Mobile = document.querySelector('.Mobile');
+  const Navigation = document.querySelector('.NavLinks');
+  const NavLinks = document.querySelectorAll('.NavLinks li');
+  const Moving = document.querySelector('.Moving');
+  const wrap = document.querySelector('.wrap');
+  const container = document.querySelector('#container');
+
+
+  Mobile.addEventListener('click',() => {
+    Navigation.classList.toggle('Navigation-active');
+    
+
+    if(Moving){
+      Moving.classList.toggle('Darkbackground');
+      wrap.classList.toggle('Darkbackground');
+    }else{
+      container.classList.toggle('Darkbackground');
+    }
+
+    NavLinks.forEach((link, index)=>{
+      if(link.style.animation){
+        link.style.animation = ''
+      } else {
+        link.getElementsByClassName.animation = `NavLinksFade 0.5s ease forwards ${index / 7 + 0.3}s`; 
+      }
+    });
+   
+    Mobile.classList.toggle('toggle');
+    
+
+  }); 
+  
+
 }
+  
+  NavigationSlide();
 
